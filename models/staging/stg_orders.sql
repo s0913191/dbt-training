c508935ea23a4cb1a169ea0d6a394dd26f9d6411
Kazuhei Sasaki <s0913191@gmail.com>
2022-05-08 01:52:31 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+SELECT
+--FROM RAW ORDERS
+o.orderid
+, o.orderdate
+, o.shipdate
+, o.shipmode
+, o.ordersellingprice - o.ordercostprice AS orderprofit
+, o.ordercostprice
+, o.ordersellingprice
+--FROM RAW CUSTOMER
+, c.customername
+, c.segment
+, c.country
+--FROM RAW PRODUCT
+, p.category
+, p.productname
+, p.subcategory
+FROM {{ ref('raw_orders') }} AS o
+LEFT JOIN {{ ref('raw_customer') }} AS c
+ON o.customerid = c.customerid
+LEFT JOIN {{ ref('raw_product') }} AS p
+ON o.productid = p.productid
